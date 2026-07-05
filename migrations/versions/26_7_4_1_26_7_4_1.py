c0aae005d2e500d1e6add158c77a9756191bc773
devsetgo <mikeryan56@gmail.com>
2026-07-05 00:33:45 +0000

    America 250 release - v26.7.4.1

---
+++ migrations/versions/26_7_4_1_26_7_4_1.py
@@ -0,0 +1,27 @@
+"""26.7.4.1
+
+Revision ID: 26_7_4_1
+Revises: 26_7_3_1
+Create Date: 2026-07-05 00:33:13.928096
+
+"""
+
+from __future__ import annotations
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '26_7_4_1'
+down_revision = '26_7_3_1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
